36b2729de41332470b1483f850716af1be196356
playatlas <atlasafri@gmail.com>
2022-11-01 06:44:01 +0300

    📄added the index page

--- messenger/chats/views.py
+++ messenger/chats/views.py
@@ -1,10 +1,15 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_GET, require_POST
 
 @require_GET
 def index(request):
-    return JsonResponse({'chats': []})
+    return HttpResponse('''<h1>Welcome to my messenger app!</h1>
+    <ul>
+        <li>Список чатов: <a href=../chats>chats/</a></li>
+        <li>Страница чата: <a href=../chats/1/>chats/1/</a></li>
+        <li>Создание чата: Проверять в <b>Postman</b></li>
+    </ul>''')
 
 @require_GET
 def chat_list(request):
